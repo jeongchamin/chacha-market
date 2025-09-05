5942c560c7082d2bc4e403a0a888f35ddc257b7d
jeongchamin <jeongchamin@gmail.com>
2025-09-05 17:36:02 +0900

    machine control

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,21 @@
 import styles from './Footer.module.css';
 
 export default function Footer() {
+  const phoneNumber = "01071174595";
+  const message = "안녕하세요! :)";
+
+  const handleClick = () => {
+    window.location.href = `sms:${phoneNumber}?body=${encodeURIComponent(message)}`;
+  };
+
   return (
     <footer className={styles.footer__area}>
       <div className='container'>
-        <div className={styles.outlet}>
-          <div className={styles.outlet__port}>
-            <div className={styles.outlet__txt}></div>
+        <div className={styles.outlet} onClick={handleClick}>
+          <div className={styles.outlet__dark}>
+            <div className={styles.outlet__port}>
+              <div className={styles.outlet__txt}></div>
+            </div>
           </div>
         </div>
         <p className={styles.footer__copyright}>
